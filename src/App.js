4003c6a299c54f2f54e1e6b604b5a5f518332134
Joe Chum <joe3719@gmail.com>
2023-03-17 10:47:29 +0800

    SomeComment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import logo from './logo.svg';
+import Header from "./header.js";
+import Nav from "./Nav.js";
+import Footer from "./Footer.js";
+import Main from "./Main.js";
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <H1>HomePage</H1>
+    <div>
+     <Helmet>
+      <meta name="description" content="your text goes here"/>
+      <meta name="og:title" content=""/>
+      <meta name="og:description" content=""/>
+      <meta name="og:image" content=""/>
+
+            </Helmet>
+    <Header />
+    <Nav />
+    <Main />
+    <Footer />
     </div>
-  );
+
+      );
 }
 
 export default App;
